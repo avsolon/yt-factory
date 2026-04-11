286ed7383e8b016b67eba43ebefe6b09dc951908
Ayesha Cyril <ayeshacy@si-b7.nsk.21-school.ru>
2026-04-11 18:32:51 +0700

    add subtitles

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import subprocess
 from openai import OpenAI
+from faster_whisper import WhisperModel
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -45,27 +47,59 @@ def tts(text):
     with open("voice.mp3", "wb") as f:
         f.write(response.content)
 
+# === 3. СУБТИТРЫ (WHISPER) ===
+def generate_subtitles():
+    model = WhisperModel("base", compute_type="int8")
+
+    segments, info = model.transcribe("voice.mp3")
+
+    with open("subtitles.srt", "w") as f:
+        for i, segment in enumerate(segments, start=1):
+            start = format_time(segment.start)
+            end = format_time(segment.end)
+            text = segment.text.strip()
+
+            f.write(f"{i}\n{start} --> {end}\n{text}\n\n")
+
+
+def format_time(seconds):
+    h = int(seconds // 3600)
+    m = int((seconds % 3600) // 60)
+    s = int(seconds % 60)
+    ms = int((seconds - int(seconds)) * 1000)
+
+    return f"{h:02}:{m:02}:{s:02},{ms:03}"
 
 # === 4. ВИДЕО ===
-# видео только внутри контейнера
+# видео без субтитров
 # def build_video():
+#     os.makedirs("output", exist_ok=True)
+#
 #     os.system("""
-#     ffmpeg -y -i assets/bg.mp4 -i voice.mp3 \
-#     -c:v copy -c:a aac -shortest final.mp4
+#     ffmpeg -y \
+#     -i assets/bg.mp4 \
+#     -i voice.mp3 \
+#     -map 0:v:0 \
+#     -map 1:a:0 \
+#     -c:v copy \
+#     -c:a aac \
+#     -shortest \
+#     output/final.mp4
 #     """)
 
-# сохранение видео вне контейнера
-
+# видео с субтитрами
 def build_video():
+    import os
     os.makedirs("output", exist_ok=True)
 
     os.system("""
     ffmpeg -y \
     -i assets/bg.mp4 \
     -i voice.mp3 \
+    -vf "subtitles=subtitles.srt:force_style='FontSize=24,PrimaryColour=&Hffffff&'" \
     -map 0:v:0 \
     -map 1:a:0 \
-    -c:v copy \
+    -c:v libx264 \
     -c:a aac \
     -shortest \
     output/final.mp4
@@ -81,9 +115,10 @@ def main():
     print("Сценарий:", script)
 
     tts(script)
+    generate_subtitles()
     build_video()
 
-    print("Готово: final.mp4")
+    print("Готово: output/final.mp4")
 
 
 if __name__ == "__main__":
